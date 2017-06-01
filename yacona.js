1f5b24cdfc7a36a0093b9dd983d9150912e63570
calmery <contact@calmery.me>
2017-06-01 22:10:01 +0900

    Add controller for app

---
+++ yacona.js
@@ -0,0 +1,63 @@
+const fs       = require( 'fs' )
+const electron = require( 'electron' )
+
+const utility = require( './utility' )
+
+const getDocumentPath = () => electron.app.getPath( 'documents' )
+const getDataPath     = () => electron.app.getPath( 'appData' )
+
+const document = {}
+const appData  = {}
+
+const save = ( basePath, name, filePath, content ) => {
+  filePath = name + '/' + filePath
+  let filePaths = filePath.split( /\/|\\/ )
+  filePaths.pop()
+
+  try {
+    filePaths.forEach( ( _, index ) => {
+      let directory = utility.getAbsolutePath( basePath, filePaths.concat().splice( 0, index + 1 ).join( '/' ) )
+      if( utility.isExist( directory ) === false )
+        fs.mkdirSync( directory )
+    } )
+    utility.write(
+      utility.getAbsolutePath( basePath, filePath ),
+      content
+    )
+  } catch( error ){
+    return false
+  }
+
+  return true
+}
+
+const load = ( basePath, name, filePath ) => {
+  basePath = utility.getAbsolutePath( basePath, name )
+  let path = utility.getAbsolutePath( basePath, filePath )
+  if( utility.isExist( path ) )
+    return utility.read( path )
+  return false
+}
+
+document.save = ( name, filePath, content ) => save( getDocumentPath(), name, filePath, content )
+document.load = ( name, filePath ) => load( getDocumentPath(), name, filePath )
+appData.save  = ( name, filePath, content ) => save( getDataPath(), name, filePath, content )
+appData.load  = ( name, filePath ) => load( getDataPath(), name, filePath )
+
+class Controller {
+
+  constructor( name ){
+    this.name = name
+    this.document = {
+      save: ( filePath, content ) => document.save( this.name, filePath, content ),
+      load: filePath => document.load( this.name, filePath )
+    }
+    this.appData = {
+      save: ( filePath, content ) => appData.save( this.name, filePath, content ),
+      load: filePath => appData.load( this.name, filePath )
+    }
+  }
+
+}
+
+module.exports = Controller
