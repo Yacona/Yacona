53dae9ba285b0db7ee76e34c5287c38e79dd4954
calmery <contact@calmery.me>
2017-06-01 15:26:26 +0900

    Add method to launch and close app

--- app.js
+++ app.js
@@ -4,8 +4,42 @@ const utility = require( './utility' )
 class App {
 
 	constructor( directory ){
+		if( directory === undefined )
+			directory = './'
+
 		this.directory = directory
-		this.package   = utility.getAbsolutePath( directory, 'yacona-package.json' )
+
+		let packageJson = utility.getAbsolutePath( directory, 'yacona-package.json' )
+		this.package = utility.isExist( packageJson )
+			? JSON.parse( utility.read( packageJson ) )
+			: {}
+
+		this.name = this.package.name !== undefined ? this.package.name : ''
+		this.app = null
+		this.isRunning = false
+
+		if( this.package.main === undefined )
+			this.package.main = 'app'
+	}
+
+	launch(){
+		if( this.isRunning )
+			return false
+		this.isRunning = true
+		this.app = require( utility.getAbsolutePath( this.directory, this.package.main ) )
+		if( this.app !== null && this.app.launch )
+			this.app.launch()
+		return true
+	}
+
+	close(){
+		if( this.isRunning ){
+			if( this.app !== null && this.app.close )
+				this.app.close()
+			this.isRunning = false
+			return true
+		}
+		return false
 	}
 
 }
