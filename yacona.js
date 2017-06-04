5860ec4747b081a09d2c3f9235a14051055a7f70
calmery <contact@calmery.me>
2017-06-04 17:32:44 +0900

    Add load and save method

--- yacona.js
+++ yacona.js
@@ -1,8 +1,24 @@
+let isElectron = false
+if( process.argv[0].split( '/' ).pop().toLowerCase() === 'electron' )
+  isElectron = true
+
 const express = require( 'express' )
 const http    = require( 'http' )
 
 const path = require( 'path' )
 const getAbsolutePath = ( ...args ) => path.resolve( path.join.apply( this, args ) )
+const isExist = filePath => {
+  try {
+    fs.statSync( path.resolve( filePath ) )
+    return true
+  } catch( error ){
+    return false
+  }
+}
+
+const fs = require( 'fs' )
+const read  = filePath => fs.readFileSync( filePath, 'utf-8' )
+const write = ( filePath, content ) => fs.writeFileSync( filePath, content )
 
 const removeRoute = ( app, path ) => {
   let stack = app._router.stack
@@ -12,6 +28,83 @@ const removeRoute = ( app, path ) => {
       break
     }
   }
+}
+
+// Use documents or appData path created by electron when we use electron command.
+// Use the current directory path when we use node command.
+// node
+// saveDocument ./documents/path/to/file
+// saveAppdata  ./appData/path/to/file
+// electron
+// saveDocument electron.app.getPath( 'documents' ) + /path/to/file
+// saveAppdata  electron.app.getPath( 'appData' ) + /path/to/file
+const savaData = ( type, chdir, filePath, content ) => {
+  let basePath = chdir
+
+  if( isElectron ){
+    const electron = require( 'electron' )
+
+    if( type === 'documents' )
+      basePath = electron.app.getPath( 'documents' )
+    else if( type === 'appData' )
+      basePath = electron.app.getPath( 'appData' )
+  } else {
+    if( type === 'documents' )
+      filePath = 'documents/' + filePath
+    else if( type === 'appData' )
+      filePath = 'appData/' + filePath
+  }
+
+  let filePaths = filePath.split( /\/|\\/ )
+  filePaths.pop()
+
+  try {
+    filePaths.forEach( ( _, index ) => {
+      let directory = getAbsolutePath( basePath, filePaths.concat().splice( 0, index + 1 ).join( '/' ) )
+      if( isExist( directory ) === false )
+        fs.mkdirSync( directory )
+    } )
+    write(
+      getAbsolutePath( basePath, filePath ),
+      content
+    )
+  } catch( error ){
+    console.log( error )
+    return false
+  }
+
+  return true
+}
+
+// Use documents or appData path created by electron when we use electron command.
+// Use the current directory path when we use node command.
+// node
+// loadDocument ./documents/path/to/file
+// loadAppdata  ./appData/path/to/file
+// electron
+// loadDocument electron.app.getPath( 'documents' ) + /path/to/file
+// loadAppdata  electron.app.getPath( 'appData' ) + /path/to/file
+const loadData = ( type, chdir, filePath ) => {
+  let basePath = chdir
+
+  if( isElectron ){
+    const electron = require( 'electron' )
+
+    if( type === 'documents' )
+      basePath = electron.app.getPath( 'documents' )
+    else if( type === 'appData' )
+      basePath = electron.app.getPath( 'appData' )
+  } else {
+    if( type === 'documents' )
+      filePath = 'documents/' + filePath
+    else if( type === 'appData' )
+      filePath = 'appData/' + filePath
+  }
+
+  let path = getAbsolutePath( basePath, filePath )
+  if( isExist( path ) )
+    return read( path )
+  return false
 }
 
 const createServer = port => {
@@ -55,6 +148,22 @@ class Yacona {
     if( chdir !== undefined )
       this.currentDirectory = chdir
     return this.currentDirectory
+  }
+
+  saveDocument( filePath, content ){
+    return savaData( 'documents', this.currentDirectory, filePath, content )
+  }
+
+  loadDocument( filePath ){
+    return loadData( 'documents', this.currentDirectory, filePath )
+  }
+
+  saveAppData(){
+    return savaData( 'appData', this.currentDirectory, filePath, content )
+  }
+
+  loadAppData( filePath ){
+    return loadData( 'appData', this.currentDirectory, filePath )
   }
 
   addClientModule( name, path ){
