08ac2ae7f92ed0cfb1bff65ae02a365e78b42a9a
Marei Kikukawa <contact@calmery.me>
2017-07-19 19:34:13 +0900

    Fix sample

--- sample/app/index.js
+++ sample/app/index.js
@@ -2,7 +2,7 @@ module.exports.launch = controller => {
   controller.addStaticRoute( './public' )
 
   controller.addSocket( 'message', ( socket, value ) => {
-    console.log( 'Message : ' + value.message )
+    console.log( 'Message : ' + value.value )
     socket.emit( 'message', { message: 'reply' } )
   } )
 
